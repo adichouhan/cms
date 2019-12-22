b50a5c22bee16d8db5329f15bebb52ed198be5d8
poonammkadam <poonammk97@gmail.com>
2019-12-21 21:44:43 -0500

    Invoice Pdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -35,7 +35,30 @@ class InvoiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $invoice = \ConsoleTVs\Invoices\Classes\Invoice::make()
+            ->addItem('Test Item', 10.25, 2, 1412)
+            ->addItem('Test Item 2', 5, 2, 923)
+            ->addItem('Test Item 3', 15.55, 5, 42)
+            ->addItem('Test Item 4', 1.25, 1, 923)
+            ->addItem('Test Item 5', 3.12, 1, 3142)
+            ->addItem('Test Item 6', 6.41, 3, 452)
+            ->addItem('Test Item 7', 2.86, 1, 1526)
+            ->addItem('Test Item 8', 5, 2, 923, 'https://dummyimage.com/64x64/000/fff')
+            ->number(4021)
+            ->with_pagination(true)
+            ->duplicate_header(true)
+            ->due_date(Carbon::now()->addMonths(1))
+            ->notes('Lrem ipsum dolor sit amet, consectetur adipiscing elit.')
+            ->customer([
+                'name'      => 'Èrik Campobadal Forés',
+                'id'        => '12345678A',
+                'phone'     => '+34 123 456 789',
+                'location'  => 'C / Unknown Street 1st',
+                'zip'       => '08241',
+                'city'      => 'Manresa',
+                'country'   => 'Spain',
+            ])
+            ->download('demo');
     }
 
     /**
