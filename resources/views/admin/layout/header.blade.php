157cb2eb82aa26d6f1761144ee3bc2f8138f24a5
poonamk97monad <poonam.kadam@monadinfotech.com>
2020-09-18 20:53:50 +0530

    add logout btn and some minor route fixes

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -28,8 +28,7 @@
         <!-- Messages Dropdown Menu -->
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge">3</span>
+                <i class="fas fa-th-large"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
 
